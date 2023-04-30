cb6ccd10d223e322a7257151baa957ee655e7424
Stanislav Kiselevskii <kisilevski@gmail.com>
2023-04-30 14:43:13 +0400

    Refactor: Extract service configuration extension Closes #21

---
+++ SimpleCounter.API/CounterServicesConfigurationExtension.cs
@@ -0,0 +1,21 @@
+﻿using SimpleCounter.Common;
+using SimpleCounter.Core;
+using SimpleCounter.Data;
+using SimpleCounter.Draw;
+
+namespace SimpleCounter.API
+{
+    public static class CounterServicesConfigurationExtension
+    {
+        public static IServiceCollection AddCountersInfrastructure(
+            this IServiceCollection services)
+        {
+            services.AddSingleton<IPageCounters, PageCounters>();
+            services.AddSingleton<ICounterDraw, CounterDraw>();
+            services.AddSingleton<ICounterData, CounterData>();
+            services.AddSingleton<ICounterCore, CounterCore>();
+
+            return services;
+        }
+    }
+}
